854640644fbfbaa17538199470d5b4708395eb94
Dev Daksan <dev.91096@gmail.com>
2020-04-01 22:51:26 +0530

    Parse the Jsonp data to state suggestions array

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,7 +8,8 @@ import styles from './styles.module.css';
 
 export class Home extends Component {
   state = {
-    query: ''
+    query: '',
+    suggestions: []
   };
 
   onQueryChange = e => {
@@ -22,19 +23,45 @@ export class Home extends Component {
       fetchJsonp(
         `https://v2.sg.media-imdb.com/suggests/${query[0]}/${query}.json`,
         {
-          jsonpCallbackFunction: `imdb$${query.replace(' ', '_')}`
+          jsonpCallbackFunction: `imdb$${query.replace(/ /g, '_')}`
         }
       )
         .then(res => {
           return res.json();
         })
         .then(json => {
-          console.log(json);
+          this.parseJSON(json);
         })
         .catch(ex => {
           console.log(ex);
         });
     }
+  };
+
+  parseJSON = json => {
+    let suggestions = [];
+
+    json.d.forEach(element => {
+      // Check if the suggestion is a movie / show
+      if ('y' in element) {
+        let image = '';
+
+        if (element.i[0] !== undefined) {
+          image = element.i[0];
+        }
+
+        suggestions.push({
+          name: element.l,
+          year: element.y,
+          id: element.id,
+          image
+        });
+      }
+    });
+
+    this.setState({
+      suggestions
+    });
   };
 
   render() {
