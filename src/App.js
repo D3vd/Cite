b76d494490773b75298ce7dfe4d2567ca1a6d793
Dev Daksan <dev.91096@gmail.com>
2020-04-01 21:09:36 +0530

    Basic component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="App"></div>;
+import Home from './components/Home';
+import Result from './components/Result';
+
+export class App extends Component {
+  state = {
+    home: true,
+    imdb_id: ''
+  };
+
+  toggleHome = () => {
+    this.setState(oldState => {
+      return {
+        home: !oldState.home
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.home ? <Home /> : <Result />}
+        <button onClick={this.toggleHome}>Change</button>
+      </div>
+    );
+  }
 }
 
 export default App;
