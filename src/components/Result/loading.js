795c63a1b48eafa29689c735f4bed7c7d8c15c55
Dev Daksan <dev.91096@gmail.com>
2020-04-02 14:45:44 +0530

    Added Loader

--- src/components/Result/loading.js
+++ src/components/Result/loading.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import styles from './loading.module.css';
+
 export class Loading extends Component {
   render() {
     return (
-      <div>
-        <h1>Loading</h1>
+      <div className={styles.container}>
+        <div className={styles.lds_ring}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <h1 className={styles.heading}>Scarping Quotes from IMDB</h1>
       </div>
     );
   }
