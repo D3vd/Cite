4dfdad2199e63969e3223035d352cb2d1baf5e57
Dev Daksan <dev.91096@gmail.com>
2020-04-02 13:39:35 +0530

    Added backend connection

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,16 +1,72 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './header';
 import Banner from './banner';
+import Quotes from './quotes';
+
+import Error from './error';
+import Loading from './loading';
 
 import styles from './styles.module.css';
 
 export class Result extends Component {
+  state = {
+    quotes: null,
+    loading: true,
+    error: ''
+  };
+
+  componentDidMount() {
+    let id = this.props.movie.id;
+
+    axios
+      .get(`https://cite-api.herokuapp.com/movie/${id}`)
+      .then(res => {
+        let data = res.data;
+
+        if (data.code !== 200) {
+          this.setState({
+            loading: false,
+            error: 'Unable to find movie with the IMDB Id'
+          });
+        } else if (data.code === 200) {
+          this.setState({
+            quotes: data.quotes,
+            loading: false
+          });
+        } else {
+          this.setState({
+            loading: false,
+            error: 'Unknown Error'
+          });
+        }
+      })
+      .catch(er => {
+        this.setState({
+          loading: false,
+          error: 'Unknown Error'
+        });
+        console.log(er);
+      });
+  }
+
   render() {
     return (
       <div className={styles.container}>
         <Header toggleHome={this.props.toggleHome} />
         <Banner movie={this.props.movie} />
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <div>
+            {this.state.error !== '' ? (
+              <Error message={this.state.error} />
+            ) : (
+              <Quotes quotes={this.state.quotes} />
+            )}
+          </div>
+        )}
       </div>
     );
   }
