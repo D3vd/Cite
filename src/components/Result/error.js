1cdbfd1f5775af6eb446c963653b0290a50ddd6f
Dev Daksan <dev.91096@gmail.com>
2020-04-02 15:06:20 +0530

    Added Error Component

--- src/components/Result/error.js
+++ src/components/Result/error.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import styles from './styles.module.css';
+
 export class Error extends Component {
   render() {
     return (
-      <div>
-        <h1>Error</h1>
+      <div className={styles.error_cnt}>
+        <img
+          className={styles.error_image}
+          src={require('../../images/page-not-found.png')}
+          alt="Error"
+        />
+        <div className={styles.error_message}>
+          {this.props.message}
+          <br />
+          Please Try Again.
+        </div>
       </div>
     );
   }
